60287c0f0088a603e0c15c6a723a775d7e97fe6f
Tii Pham <tiipham19@gmail.com>
2022-08-26 21:40:43 -0500

    Added photos

--- src/pages/LookBook.jsx
+++ src/pages/LookBook.jsx
@@ -11,7 +11,7 @@ function LBImages (props) {
             <img 
                 className="lb-images" 
                 src={props.src} 
-                alt="alt-book"
+                alt="5-star-nail-spa-nails-look"
                 onClick={()=>setShow(!show)}
             />            
             
@@ -19,7 +19,7 @@ function LBImages (props) {
             {/* Modal Images */}
             <div className="overlay" style={{display: show ? "inline-block" : "none"}}>
                 <div className="img-modal-lb">
-                    <img className="lb-images-modal" src={props.src} alt="alt-book"/>
+                    <img className="lb-images-modal" src={props.src} alt="5-star-nail-spa-nails-look"/>
                     
                     <div className="bottom">
                         
@@ -44,7 +44,7 @@ function LookBook (props) {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        setTimeout(() => setLoading(false), 2500)
+        setTimeout(() => setLoading(false), 1500)
         window.top.document.title = "5 Star Nail Spa | Look Book"
         window.addEventListener("scroll", () => {
             const winheight = window.scrollY;
@@ -56,18 +56,18 @@ function LookBook (props) {
         {id: 1, src: require('../images/1.png'), artist:'Kavin'} ,
         {id: 2, src: require('../images/2.png'), artist:'Silva'},
         {id: 3, src: require('../images/3.png'), artist:'Silva'},
-        {id: 4, src: require('../images/4.png'), artist:'Silva'},
+        {id: 4, src: require('../images/4.png'), artist:'Mimi'},
         {id: 5, src: require('../images/5.png'), artist:''},
         {id: 6, src: require('../images/6.png'), artist:'Holly'},
         {id: 7, src: require('../images/7.png'), artist:'Vivian'},
-        {id: 8, src: require('../images/8.png'), artist:''},
-        {id: 9, src: require('../images/9.png'), artist:'Vivian'},
-        {id: 10, src: require('../images/10.png'), artist:''},
-        {id: 11, src: require('../images/11.png'), artist:''},
-        {id: 12, src: require('../images/12.png'), artist:''},
+        {id: 8, src: require('../images/8.png'), artist:'Silva'},
+        {id: 9, src: require('../images/9.png'), artist:'Silva'},
+        {id: 10, src: require('../images/10.png'), artist:'Vivian'},
+        {id: 11, src: require('../images/11.png'), artist:'Mimi'},
+        {id: 12, src: require('../images/12.png'), artist:'Mimi'},
         {id: 13, src: require('../images/13.png'), artist:''},
         {id: 14, src: require('../images/14.png'), artist:''},
-        {id: 15, src: require('../images/15.png'), artist:''},
+        {id: 15, src: require('../images/15.png'), artist:'Amy'},
         {id: 16, src: require('../images/16.png'), artist:''},
         {id: 17, src: require('../images/17.png'), artist:'Silva'},
         {id: 18, src: require('../images/18.png'), artist:'Judy'},
@@ -77,14 +77,14 @@ function LookBook (props) {
         {id: 22, src: require('../images/22.png'), artist:'Silva'},
         {id: 23, src: require('../images/23.png'), artist:''},
         {id: 24, src: require('../images/24.png'), artist:''},
-        {id: 25, src: require('../images/25.png'), artist:''},
-        {id: 26, src: require('../images/26.png'), artist:''},
+        {id: 25, src: require('../images/25.png'), artist:'Mimi'},
+        {id: 26, src: require('../images/26.png'), artist:'Holly'},
         {id: 27, src: require('../images/27.png'), artist:''},
         {id: 28, src: require('../images/28.png'), artist:''},
         {id: 29, src: require('../images/29.png'), artist:'Holly'},
         {id: 30, src: require('../images/30.png'), artist:''},
         {id: 31, src: require('../images/31.png'), artist:''},
-        {id: 32, src: require('../images/32.png'), artist:''},
+        {id: 32, src: require('../images/32.png'), artist:'Silva'},
         {id: 33, src: require('../images/33.png'), artist:''},
         {id: 34, src: require('../images/34.png'), artist:'Judy'},
         {id: 35, src: require('../images/35.png'), artist:''},
@@ -98,7 +98,7 @@ function LookBook (props) {
         {id: 43, src: require('../images/43.png'), artist:'Holly'},
         {id: 44, src: require('../images/44.png'), artist:'Tony'},
         {id: 45, src: require('../images/45.png'), artist:'Holly'},
-
+        {id: 46, src: require('../images/46.png'), artist:'Silva'},
     ];
 
     
