4a1afe8b3852a0991c70d700d7b09fa4af0004ee
Tii Pham <tiipham19@gmail.com>
2022-12-18 04:22:06 -0600

    changed nav button

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -53,12 +53,18 @@ const NavBar = () => {
     (
       <div className="nav-body">
         <nav>
-            <button onClick={()=> setShow(!show)} onMouseOver={()=> setShow(!show)} id="ham-button" className="ham-btn">&#9776;</button>
+            <div id="ham-button" className={show ? "ham-btn ham-active" : "ham-btn"} onClick={()=> setShow(!show)}>
+                <div className="div1"></div>
+                <div className="div2"></div>
+                <div className="div3"></div>
+            </div>
+            {/* <button onClick={()=> setShow(!show)} onMouseOver={()=> setShow(!show)} id="ham-button" className="ham-btn">&#9776;</button> */}
             <img className="logo" src="images/LOGO.svg" alt="logo"/>
             <ul id="hamburger" className="nav" style={{maxHeight: show ? "500px" : "0px" }}>
                 <NavLink
                         to="/"
-                        className="nav-item">
+                        className="nav-item"
+                        onClick={()=> setShow(!show)}>
                         Home
                 </NavLink>
                 
